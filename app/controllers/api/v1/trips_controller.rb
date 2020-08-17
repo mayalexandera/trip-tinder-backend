e3ee26338f5de46c6d89bdf796fbf0782845ba9a
mayalexandera <maya.blevins@gmail.com>
2020-08-17 12:37:12 -0400

    fix auth

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -8,28 +8,20 @@ class Api::V1::TripsController < ApplicationController
   end
 
   def create
-    @user = User.find(id: @user.id)
-    @trip = Trip.create(trip_params)
-    if @trip.save
-      UserTrip.create!(
-        trips_id: @trip.id,
-        trip_lead: @trip.trip_lead
-      )
-
-      ParkTrip.create!(
-        trip_id: @trip.id,
-        park_id: @trip.park_id
-      )
-    end
-
-    if !@trip.valid?
-      render json: { error: 'failed to create trip'}, status: :not_acceptable
-    end
+    @user = User.find_by(username: params[:trip_lead][:username])
+    @trip = Trip.new(trip_params)
+    @trip.trip_lead = @user
+    @trip.save!
+      if @trip.save
+       render json: @trip
+      else
+        render json: { error: 'failed to create trip'}, status: :not_acceptable
+      end
   end
 
   private
 
   def trip_params
-    params.require(:trip).permit(:name, :state, :description, :start_date, :end_date, :park_id, :trip_lead, :difficulty_rating)
+    params.require(:trip).permit(:name, :state, :description, :start_date, :end_date, :park_id, :trip_goers, :trip_lead, :difficulty_rating)
   end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,11 +1,10 @@
 class Trip < ApplicationRecord
   belongs_to :trip_lead, class_name: 'User'
   belongs_to :park
-  has_many :trip_goers, class_name: 'User'
+  has_many :trip_goers, through: :user_trips
 
   validates :name, length: { minimum: 4 }, presence: true
   validates :start_date, :end_date, :trip_lead, :park_id, :description, :state, presence: true
   validates :difficulty_rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
-  validates :trip_goers, presence: true, allow_nil: true
 
 end
