cfbb7ceec11f11c7107c2efc8b568143d1a252a3
dr-dolce14 <dolcemascop1@gmail.com>
2020-08-19 11:40:25 -0400

    data seeded

--- db/schema.rb
+++ db/schema.rb
@@ -29,18 +29,6 @@ ActiveRecord::Schema.define(version: 2020_08_18_153020) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "park_trips", force: :cascade do |t|
     t.bigint "trip_id"
     t.bigint "park_id"
@@ -98,7 +86,6 @@ ActiveRecord::Schema.define(version: 2020_08_18_153020) do
     t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "roles"
   end
 
   add_foreign_key "park_trips", "parks"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,50 @@
 
 
 
+# Trip.create!(
+#   name: Faker::Lorem.sentences(number: 1),
+#   park: Park.first,
+#   state: Park.first.state,
+#   description: Faker::Lorem.sentences(number: 3),
+#   start_date: Faker::Date.in_date_period(year: 2021, month: 6),
+#   end_date: Faker::Date.in_date_period(year: 2021, month: 7),
+#   trip_lead: User.first,
+#   difficulty_rating: 3
+# )
+
 Trip.create!(
-  name: Faker::Lorem.sentences(number: 1),
+  name: "How about stopping to smell the roses??",
   park: Park.first,
   state: Park.first.state,
-  description: Faker::Lorem.sentences(number: 3),
+  description: "Getting tired of sitting in your small apartment? Join us on this fun-filled hiking adventure and meet like-minded people looking to unplug and unwind! There will be hiking and camping opportunities available!" ,
   start_date: Faker::Date.in_date_period(year: 2021, month: 6),
   end_date: Faker::Date.in_date_period(year: 2021, month: 7),
   trip_lead: User.first,
-  difficulty_rating: 3
+  difficulty_rating: 2
 )
+
+Trip.create!(
+  name: "Serious outdoor enthusiasts only!",
+  park: Park.second,
+  state: Park.second.state,
+  description: "This trip will be no joke! Serious mountain biking and whitewater rafting...and serious drinking by the campfire!" ,
+  start_date: Faker::Date.in_date_period(year: 2021, month: 5),
+  end_date: Faker::Date.in_date_period(year: 2021, month: 6),
+  trip_lead: User.second,
+  difficulty_rating: 4
+)
+
+Trip.create!(
+  name: "Anyone want to do some rock climbing?",
+  park: Park.third,
+  state: Park.third.state,
+  description: "Getting tired of sitting in your small apartment? Join us on this fun-filled hiking adventure and meet like-minded people looking to unplug and unwind! There will be hiking and camping opportunities available!" ,
+  start_date: Faker::Date.in_date_period(year: 2021, month: 8),
+  end_date: Faker::Date.in_date_period(year: 2021, month: 9),
+  trip_lead: User.third,
+  difficulty_rating: 5
+)
+
 
 
 ParkTrip.create!(
@@ -18,14 +52,80 @@ ParkTrip.create!(
   park_id: Trip.first.park.id
 )
 
-10.times do User.create!(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  username: Faker::Internet.username(specifier: 5..14), 
-  password: Faker::Internet.password,
-  bio: Faker::Lorem.paragraphs(number: 1),
-  avatar: Faker::Avatar.image
+ParkTrip.create!(
+  trip_id: Trip.second.id,
+  park_id: Trip.second.park.id
+)
+
+ParkTrip.create!(
+  trip_id: Trip.third.id,
+  park_id: Trip.third.park.id
 )
 
 
-end
+
+
+# 10.times do User.create!(
+#   first_name: Faker::Name.first_name,
+#   last_name: Faker::Name.last_name,
+#   username: Faker::Internet.username(specifier: 5..14), 
+#   password: Faker::Internet.password,
+#   bio: Faker::Lorem.paragraphs(number: 1),
+#   avatar: Faker::Avatar.image
+# )
+
+
+# end
+
+User.create!(
+  first_name: "Tina",
+  last_name: "TheCat",
+  username: "CuteKitty",
+  password: 12345,
+  bio: "I am a cute, if slightly annoying cat, who surprisingly loves water. My favorite thing to do is go whitewater rafting. Also, I will put my butthole in your face.",
+  avatar: "https://www.washingtonian.com/wp-content/uploads/2019/02/milada-vigerova-1295750-unsplash-2048x3072.jpg"
+)
+
+
+
+User.create!(
+  first_name: "Korra"
+  last_name: "TheAvatar"
+  username: "Waterbender"
+  password: 12345
+  bio: "I am the Avatar. I kind of have super powers. But I'm also super chill. I like extreme sports.",
+  avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Korra_%28Season_4%29_appearance.jpg/220px-Korra_%28Season_4%29_appearance.jpg"
+)
+
+User.create!(
+  first_name: "Bette"
+  last_name: "Porter"
+  username: "Pantsuits"
+  password: 12345
+  bio: "Don't mess with me. But I'm nice, really. I love the outdoors and the environment. I will hurt you if you don't believe in climate change."
+  avatar: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/elle-jennifer-beals-1580006792.jpg"
+)
+
+User.create!(
+  first_name: "Tom"
+  last_name: "Hanks"
+  user_name: "ForrestGump"
+  password: 12345,
+  bio: "Everyone loves me."
+  avatar: "https://www.biography.com/.image/t_share/MTE1ODA0OTcxNjUxNTk3ODM3/tom-hanks-9327661-1-402.jpg"
+)
+
+UserTrip.create!(
+  trip_id: Trip.first.id
+  user_id: User.first.id 
+)
+
+UserTrip.create!(
+  trip_id: Trip.second.id
+  user_id: User.second.id
+)
+
+UserTrip.create!(
+  trip_id: Trip.third.id 
+  user_id: User.third.id
+)
